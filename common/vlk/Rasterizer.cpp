1dcf17432e7531b66e4fa2356f24a33ea4bc56a7
Roman Divotkey <roman.divotkey@gmail.com>
2021-01-22 08:52:29 +0100

    Work continued.

--- common/vlk/Rasterizer.cpp
+++ common/vlk/Rasterizer.cpp
@@ -11,15 +11,41 @@ using namespace std;
 
 RasterizerBuilder::RasterizerBuilder()
 {
-    
+    Reset();
+}
+
+RasterizerBuilder & RasterizerBuilder::EnableDepthClamp(bool value)
+{
+    depthClamp = value;
+    polygonMode = VK_POLYGON_MODE_FILL;
+    return *this;
+}
+
+RasterizerBuilder & RolygonMode(VkPolygonMode mode)
+{
+    polygonMode = mode;
+    return *this;
+}
+
+RasterizerBuilder & LineWidth(float width)
+{
+    lineWidth = width;
+    return *this;
 }
 
 RasterizerBuilder& RasterizerBuilder::Reset()
 {
-
+    depthClamp = false;
+    lineWidth = 1.0f;
 }
 
 VkPipelineRasterizationStateCreateInfo RasterizerBuilder::Build()
 {
+    VkPipelineRasterizationStateCreateInfo rasterizer{};
+    rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizer.depthClampEnable = depthClamp ? VK_TRUE : VK_FALSE;
+    rasterizer.polygonMode = polygonMode;
+    rasterizer.lineWidth = lineWidth;
 
+    return rasterizer;
 }
--- common/vlk/Rasterizer.h
+++ common/vlk/Rasterizer.h
@@ -15,11 +15,65 @@
 class RasterizerBuilder {
 public:
 
+    /**
+     * Constructor.
+     */
     RasterizerBuilder();
 
+
+    /**
+     * Specified whether to enable depth clamp.
+     * 
+     * If depthClamp is enabled, then fragments that are beyond the near and
+     * far planes are clamped to them as opposed to discarding them.
+     * This is useful in some special cases like shadow maps.
+     * Using this requires enabling a GPU feature.
+     * 
+     * @param value `true` to enable depth clamp.
+     * @return reference to this builder used for method chaining
+     */
+    RasterizerBuilder & EnableDepthClamp(bool value);
+
+    /**
+     * Specifies the polygon rasterization mode.
+     * 
+     * @param mode  the polygon rasterization mode
+     * @return reference to this builder used for method chaining
+     */
+    RasterizerBuilder & RolygonMode(VkPolygonMode mode);
+
+    /**
+     * Specifies the width of rasterized line segments.
+     * 
+     * @param width the width of line segments
+     * @return reference to this builder used for method chaining
+     */
+    RasterizerBuilder & LineWidth(float width);
+
+
+    /**
+     * Resets this builder to its initial condition.
+     * 
+     * @return reference to this builder used for method chaining
+     */
     RasterizerBuilder& Reset();
 
+    /**
+     * Builds the rasterizer structure according to the current configuration.
+     * 
+     * @return the rasterizer structure
+     * @throws std::logic_error in case the current configuration is invalid
+     */
     VkPipelineRasterizationStateCreateInfo Build();
 
 private:
+
+    /** Whether to enable depth clamp of fragments. */
+    bool depthClamp;
+
+    /** Control polygon rasterization mode. */
+    VkPolygonMode polygonMode;
+
+    /** The width of rasterized line segments.*/
+    double lineWidth;
 };
